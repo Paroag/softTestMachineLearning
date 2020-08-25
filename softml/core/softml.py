23f19a44285fc6705ad07fb335727bbc9a9a3db1
Your Name <you@example.com>
2020-08-25 17:20:04 +0200

    [add] polynomial regression (configurable) as a standard model

--- softml/core/softml.py
+++ softml/core/softml.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 
 from softml.util.utils import nested_dictionnary
@@ -13,6 +14,22 @@ def evaluate_linear_regression(X_train, Y_train, X_val, Y_val, scaler = None) :
     linear_regressor = LinearRegression()
     linear_regressor.fit(X_train, Y_train)
     Y_pred = linear_regressor.predict(X_val)
+
+    if scaler :
+        Y_pred = scaler.inverse_transform(Y_pred) 
+
+    return(mean_squared_error(Y_pred, Y_val))
+
+def evaluate_polynomial_regression(X_train, Y_train, X_val, Y_val, degree = 2, scaler = None) :
+
+    polynomial_features= PolynomialFeatures(degree = degree, include_bias = False)
+    X_train_poly = polynomial_features.fit_transform(X_train)
+    X_val_poly = polynomial_features.fit_transform(X_val)
+
+    linear_regressor = LinearRegression()
+
+    linear_regressor.fit(X_train_poly, Y_train)
+    Y_pred = linear_regressor.predict(X_val_poly)
 
     if scaler :
         Y_pred = scaler.inverse_transform(Y_pred) 
@@ -66,7 +83,7 @@ def normalize_data(X_train, X_val, Y_train, scalers) :
 
     return(X_train_normalized, X_val_normalized, Y_train_normalized)
 
-def soft_test_dataframe(df, Y_columns, train_size = 0.75, random_state = None) :
+def soft_test_dataframe(df, Y_columns, polynomial_regression_degree = 2, train_size = 0.75, random_state = None) :
 
     response = {}
 
@@ -79,25 +96,64 @@ def soft_test_dataframe(df, Y_columns, train_size = 0.75, random_state = None) :
     X_train_standardized, X_val_standardized, Y_train_standardized = standardize_data(X_train, X_val, Y_train, scalers)
     X_train_normalized, X_val_normalized, Y_train_normalized = normalize_data(X_train, X_val, Y_train, scalers)
 
+    #####
+    #
+    # LINEAR REGRESSION
+    #
+    #####
+
     mse_LR = evaluate_linear_regression(
         X_train, Y_train, X_val, Y_val, 
         scaler = None)
+    nested_dictionnary(response, ["LinearRegression","NoScaler", "mse"], mse_LR)
 
     mse_LR_standardized = evaluate_linear_regression(
         X_train_standardized, Y_train_standardized, X_val_standardized, Y_val, 
         scaler = scalers["StandardScaler"]["Y"])
+    nested_dictionnary(response, ["LinearRegression","StandardScaler", "mse"], mse_LR_standardized)
 
     mse_LR_normalized = evaluate_linear_regression(
         X_train_normalized, Y_train_normalized, X_val_normalized, Y_val, 
         scaler = scalers["MinMaxScaler"]["Y"])
+    nested_dictionnary(response, ["LinearRegression","MinMaxScaler", "mse"], mse_LR_normalized)
 
-    mse_RF = evaluate_random_forest(X_train, Y_train, X_val, Y_val, random_state = random_state)
+    #####
+    #
+    # POLYNOMIAL REGRESSION
+    #
+    #####
 
+    for degree in range(2, polynomial_regression_degree+1, 1) :
+
+        mse_PR = evaluate_polynomial_regression(
+            X_train, Y_train, X_val, Y_val, 
+            scaler = None,
+            degree = degree)
+        nested_dictionnary(response, ["PolynomialRegression","NoScaler", f"degree = {degree}","mse"], mse_PR)
+
+        mse_PR_standardized = evaluate_polynomial_regression(
+            X_train_standardized, Y_train_standardized, X_val_standardized, Y_val, 
+            scaler = scalers["StandardScaler"]["Y"],
+            degree = degree)
+        nested_dictionnary(response, ["PolynomialRegression","StandardScaler", f"degree = {degree}", "mse"], mse_PR_standardized)
+
+        mse_PR_normalized = evaluate_polynomial_regression(
+            X_train_normalized, Y_train_normalized, X_val_normalized, Y_val, 
+            scaler = scalers["MinMaxScaler"]["Y"],
+            degree = degree)
+        nested_dictionnary(response, ["PolynomialRegression","MinMaxScaler", f"degree = {degree}", "mse"], mse_PR_normalized)
+
+    #####
+    #
+    # RANDOM FOREST
+    #
+    #####
+
+    mse_RF = evaluate_random_forest(
+        X_train, Y_train, X_val, Y_val,
+        random_state = random_state)
     nested_dictionnary(response, ["RandomForest","NoScaler", "mse"], mse_RF)
 
-    nested_dictionnary(response, ["LinearRegression","NoScaler", "mse"], mse_LR)
-    nested_dictionnary(response, ["LinearRegression","StandardScaler", "mse"], mse_LR_standardized)
-    nested_dictionnary(response, ["LinearRegression","MinMaxScaler", "mse"], mse_LR_normalized)
 
     return response
 
--- tests/test_softml.py
+++ tests/test_softml.py
@@ -5,7 +5,9 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from softml.core.softml import evaluate_linear_regression, evaluate_random_forest
+from softml.core.softml import evaluate_linear_regression
+from softml.core.softml import evaluate_random_forest
+from softml.core.softml import evaluate_polynomial_regression
 from softml.core.softml import input_output_split
 from softml.core.softml import soft_test_dataframe
 from softml.core.softml import generate_scalers
@@ -23,7 +25,7 @@ class SoftMLTest(unittest.TestCase):
             self.df = pd.read_csv("../resources/SwedishInsuranceDataset/AutoInsurSweden_reformated.txt",\
                  sep = "\t", header = None)
 
-            self.df2 = pd.read_csv("../resources/BostonHousingDataset/housing.data",\
+            self.df_BHD = pd.read_csv("../resources/BostonHousingDataset/housing.data",\
                  delim_whitespace=True, header = None)
 
             self.X = self.df.iloc[:, 0].values.reshape(-1, 1)
@@ -47,9 +49,29 @@ class SoftMLTest(unittest.TestCase):
         mse = evaluate_linear_regression(self.X_train_standardized, self.Y_train_standardized, self.X_val_standardized, self.Y_val, scaler = self.scalers["StandardScaler"]["Y"])
         self.assertAlmostEqual(mse, 1499.5468481994199)
 
-        # MSE with standardization
+        # MSE with normalization
         mse = evaluate_linear_regression(self.X_train_normalized, self.Y_train_normalized, self.X_val_normalized, self.Y_val, scaler = self.scalers["MinMaxScaler"]["Y"])
         self.assertAlmostEqual(mse, 1499.5468481994199)
+
+    def test_evaluate_polynomial_regression(self) :
+
+        # MSE with no scaler - degree = 2
+        mse = evaluate_polynomial_regression(
+            self.X_train, self.Y_train, self.X_val, self.Y_val, 
+            degree = 2)
+        self.assertAlmostEqual(mse, 1441.9536333497103)
+
+        # MSE with standardization - degree = 2
+        mse = evaluate_polynomial_regression(
+            self.X_train_standardized, self.Y_train_standardized, self.X_val_standardized, self.Y_val, 
+            degree = 2, scaler = self.scalers["StandardScaler"]["Y"])
+        self.assertAlmostEqual(mse, 1441.9536333497101)
+
+        # MSE with standardization - degree = 3
+        mse = evaluate_polynomial_regression(
+            self.X_train_normalized, self.Y_train_normalized, self.X_val_normalized, self.Y_val, 
+            degree = 3, scaler = self.scalers["MinMaxScaler"]["Y"])
+        self.assertAlmostEqual(mse, 1377.0062199850454)
 
     def test_evaluate_random_forest(self) :
 
@@ -80,13 +102,19 @@ class SoftMLTest(unittest.TestCase):
         self.assertAlmostEqual(response["LinearRegression"]["NoScaler"]["mse"], 1499.5468481994199)
         self.assertAlmostEqual(response["RandomForest"]["NoScaler"]["mse"], mse_RF)
 
-    def test_soft_test_dataframe_peformance(self) :
+    def test_soft_test_dataframe_performance(self) :
 
         # Performance validation on BostonHousingDataset
-        # Performance is compared to 
-        # https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
 
-        response = soft_test_dataframe(self.df2, 13, train_size = self.TRAIN_SIZE, random_state = self.random_state)
+        # Performance provided by
+        # https://machinelearningmastery.com/regression-tutorial-keras-deep-learning-library-python/
+        # is MSE +/-= 24 - 27 (NN)
+
+        # Performance provided by
+        # https://towardsdatascience.com/linear-regression-on-boston-housing-dataset-f409b7e4a155
+        # is MSE +/-= 26
+
+        response = soft_test_dataframe(self.df_BHD, 13, train_size = self.TRAIN_SIZE, random_state = self.random_state)
 
         self.assertAlmostEqual(response["LinearRegression"]["StandardScaler"]["mse"], 29.782245092302368)
         self.assertAlmostEqual(response["LinearRegression"]["MinMaxScaler"]["mse"], 29.782245092302432)
